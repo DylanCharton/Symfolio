5d880cfbb4dffd94fd8f5b3fcfe89d34a6eb2d3d
DylanCharton <d.charton@codeur.online>
2021-12-13 15:02:17 +0100

    trying to put online

--- Symfolio/src/Controller/ContactController.php
+++ Symfolio/src/Controller/ContactController.php
@@ -6,7 +6,6 @@ use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
